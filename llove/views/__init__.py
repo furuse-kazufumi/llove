7bed37a5002f8974a980cc0f3a9f6622527b6448
furuse-kazufumi <kazufumi@furuse.work>
2026-05-10 10:15:11 +0900

    auto: CHANGELOG.md 編集前 (2026-05-10 10:15)

--- llove/views/__init__.py
+++ llove/views/__init__.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from .audit_log import AuditLogView
 from .base import View
+from .folding import FoldRegion, FoldState, apply_folds, find_heading_regions
 from .markdown_view import MarkdownView
 from .narration import NarrationView
 from .sensor_stream import SensorStreamView
@@ -11,9 +12,13 @@ from .spc_chart import SPCChartView
 
 __all__ = [
     "AuditLogView",
+    "FoldRegion",
+    "FoldState",
     "MarkdownView",
     "NarrationView",
     "SPCChartView",
     "SensorStreamView",
     "View",
+    "apply_folds",
+    "find_heading_regions",
 ]
