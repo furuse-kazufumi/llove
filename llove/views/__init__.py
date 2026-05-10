60e1f4f9802b4deb8215459228fe559ccecc792b
furuse-kazufumi <kazufumi@furuse.work>
2026-05-10 12:09:59 +0900

    auto: test_mermaid_render.py 編集前 (2026-05-10 12:09)

--- llove/views/__init__.py
+++ llove/views/__init__.py
@@ -19,6 +19,14 @@ from .folding_persistence import (
     save_fold_state,
 )
 from .markdown_view import MarkdownView
+from .mermaid_render import (
+    MermaidRender,
+    ascii_fallback,
+    find_image_tool,
+    mmdc_available,
+    render_mermaid,
+    render_mermaid_to_svg,
+)
 from .narration import NarrationView
 from .sensor_stream import SensorStreamView
 from .spc_chart import SPCChartView
@@ -29,15 +37,21 @@ __all__ = [
     "FoldRegion",
     "FoldState",
     "MarkdownView",
+    "MermaidRender",
     "NarrationView",
     "SPCChartView",
     "SensorStreamView",
     "View",
     "apply_folds",
+    "ascii_fallback",
     "default_fold_state_path",
     "find_code_block_regions",
     "find_heading_regions",
+    "find_image_tool",
     "find_table_regions",
     "load_fold_state",
+    "mmdc_available",
+    "render_mermaid",
+    "render_mermaid_to_svg",
     "save_fold_state",
 ]
