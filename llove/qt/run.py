299db1ee9f39ccd20de6c00b120b3b870dbaf441
furuse-kazufumi <kazufumi@furuse.work>
2026-06-29 21:23:11 +0900

    auto: run.py 編集前 (2026-06-29 21:23)

--- llove/qt/run.py
+++ llove/qt/run.py
@@ -17,7 +17,7 @@ from PySide6 import QtWidgets
 
 from llove.qt.fitness_panel import FitnessTrajectoryPanel
 from llove.qt.qd_archive_panel import QdArchivePanel
-from llove.qt.worker import MetricsTailController
+from llove.qt.worker import JsonlTailController, MetricsTailController
 
 
 def build_window(
